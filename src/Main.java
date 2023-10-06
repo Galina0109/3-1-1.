244ccacc08256130407544a27e532cc03a80665c
Galina <gana88tit@gmail.com>
2023-10-06 10:32:12 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 15225; // стоимость билета
+        int miles = 20; // количество рублей для одной бонусной мили
+
+
+        System.out.println("За каждые 20 рублей, потраченных на билет начислется:");
+        System.out.println((price / miles) + " миля/(мили)");
+
+    }
+}
